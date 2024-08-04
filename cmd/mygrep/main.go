d3b30cb7da4376888ac06d4f3e7ce3938bebfbc8
Andrew Mushel <almushel@protonmail.com>
2024-08-04 14:44:31 -0500

    codecrafters submit [skip ci]

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+
 	//	"strings"
 	"unicode/utf8"
 )
@@ -40,7 +41,10 @@ func main() {
 func matchLine(line []byte, pattern string) (bool, error) {
 	var ok bool
 	plen := utf8.RuneCountInString(pattern)
-	if plen == 2 {
+
+	if plen == 1 {
+		ok = bytes.ContainsAny(line, pattern)
+	} else if plen == 2 {
 		switch pattern[1] {
 		case 'w':
 			ok = bytes.ContainsFunc(bytes.ToLower(line), func(r rune) bool {
@@ -52,8 +56,9 @@ func matchLine(line []byte, pattern string) (bool, error) {
 			break
 
 		}
-	} else if plen == 1 {
-		ok = bytes.ContainsAny(line, pattern)
+	} else if pattern[0] == '[' && pattern[len(pattern)-1] == ']' {
+		group := pattern[1 : len(pattern)-1]
+		ok = bytes.ContainsAny(line, group)
 	} else {
 		return false, fmt.Errorf("unsupported pattern: %q", pattern)
 	}
